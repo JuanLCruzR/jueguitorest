137f60bf6a47095e155b9eeeb677a53ce27e1e83
dannielXdx <dannielxdx@outlook.com>
2021-10-31 17:21:07 -0600

    Se sube Palos panas models.py

--- jueguito/aplicaciones/erona/admin.py
+++ jueguito/aplicaciones/erona/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from aplicaciones.erona.models import *
 
 # Register your models here.
+admin.site.register(Padawan)
+admin.site.register(Area)
+admin.site.register(Jedi)
+admin.site.register(Erona)
--- jueguito/aplicaciones/erona/models.py
+++ jueguito/aplicaciones/erona/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class Padawan(models.Model):
+    idUser = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+
+    def __str__(self):
+        txt = "nombre: {0} / usuario: {1}"
+        return txt.format(self.nombre, self.username)
+
+
+class Area(models.Model):
+    id_Areas = models.AutoField(primary_key=True)
+    area = models.CharField(max_length=30, default='Ejemplo: Back / Front / Devs')
+    orientado = models.CharField(max_length=100, null=False, blank=True)
+    descripcion = models.CharField(max_length=100, null=False, blank=True)
+    conocimiento = models.CharField(max_length=100, null=False, blank=True)
+
+    def __str__(self):
+        txt = "{0}"
+        return txt.format(self.area)
+
+class Jedi(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    area = models.ForeignKey(Area, null=False, blank=False, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        txt = "Jedi: {0} / Area: {1}"
+        return txt.format(self.nombre, self.area)
+
+class Erona(models.Model):
+    usuario = models.ForeignKey(Padawan, null=False, blank=False, on_delete=models.CASCADE)
+    id_Jedi = models.ForeignKey(Jedi, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        txt = "[Padawan: {0}] de [{1}]"
+        return txt.format(self.usuario, self.id_Jedi)
